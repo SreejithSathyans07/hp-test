e0819f7bae078e5704ccd7478074dd3ecf9702b8
Sreejith <sreejithsathyans@gmail.com>
2021-05-02 23:19:08 +0530

    splunk changes

--- script.js
+++ script.js
@@ -10,21 +10,26 @@ function Calculate(){
 
     var division = a/b;
     console.log("Division = ",division);
+	console.log("Splunk method name division");
+	
 }
 
 function sum(a, b){
+	console.log("Splunk method name sum");
     //This is added as part of devcon-1664
     var sum = +a+b;
     console.log("sum = "+sum);
 }
 
 function difference(a, b){
+	console.log("Splunk method name differece");
     //This is added as part of devcon-1707
     var difference = +a-b;
     console.log("Differnce = "+difference);
 }
 
 function product(a, b){
+	console.log("Splunk method name product");
     //This is added as part of devcon-1855
     var product = a*b;
     console.log("Product = ",product);
