221db7311ba31b5932b3c93d2e84b16951e5f389
SreejithSathyans07 <60210194+SreejithSathyans07@users.noreply.github.com>
2021-05-02 22:35:25 +0530

    Adding files to the repository
    Initial two files are added

---
+++ script.js
@@ -0,0 +1,18 @@
+function Calculate(){
+    var a = 10;
+    var b = 5;
+
+    var sum = +a+b;
+    console.log("sum = "+sum);
+
+    var difference = +a-b;
+    console.log("Differnce = "+difference);
+
+    var product = a*b;
+    console.log("Product = ",product);
+
+    var division = a/b;
+    console.log("Division = ",division);
+}
+
+Calculate();
