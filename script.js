811ea85ac1d8f0b4cf4426b1063aa97d5ae1e91f
Sreejith <sreejithsathyans@gmail.com>
2021-05-02 22:59:31 +0530

    DEVCON-1707 Adding the difference method

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function Calculate(){
     var sum = +a+b;
     console.log("sum = "+sum);
 
-    var difference = +a-b;
-    console.log("Differnce = "+difference);
+    difference(a,b);
 
     var product = a*b;
     console.log("Product = ",product);
@@ -15,4 +14,18 @@ function Calculate(){
     console.log("Division = ",division);
 }
 
+<<<<<<< Updated upstream
+=======
+function sum(a, b){
+    var sum = +a+b;
+    console.log("sum = "+sum);
+}
+
+function difference(a, b){
+    //This is added as part of devcon-1707
+    var difference = +a-b;
+    console.log("Differnce = "+difference);
+}
+
+>>>>>>> Stashed changes
 Calculate();
